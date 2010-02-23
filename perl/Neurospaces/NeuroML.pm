bc2cabad167ce88d4d69345a653b57d0a367cff3
Hugo Cornelis <hugo.cornelis@gmail.com>
2010-02-23 16:46:20 +0000

    1. Added .gdbinit. 2. NeuroML parsing now works, no mapping yet.

--- perl/Neurospaces/NeuroML.pm
+++ perl/Neurospaces/NeuroML.pm
@@ -5,4 +5,74 @@
 use strict;
 
 
+use Neurospaces;
+
+use XML::Simple;
+# use XML::Twig;
+
+
+package Neurospaces::NeuroML::Parser;
+
+
+sub new
+{
+    my $package = shift;
+
+    my $options = shift || {};
+
+    my $model_container = Neurospaces->new();
+
+    $model_container->read(undef, [ "/usr/local/neurospaces/models/library/utilities/empty_model.ndf", ]);
+
+    my $self
+	= {
+	   model_container => $model_container,
+# 	   xml_twig => XML::Twig->new();
+	   %$options,
+	  };
+
+    bless $self, $package;
+
+    return $self;
+}
+
+
+sub read
+{
+    my $self = shift;
+
+    my $filename = shift;
+
+    $self->{filename} = $filename;
+
+    my $qualified_filename = $self->qualify($filename);
+
+    $self->{qualified_filename} = $qualified_filename;
+
+#     $self->{xml_twig}->parsefile($qualified_filename);
+
+    $self->{xml_simple} = XML::Simple::XMLin($qualified_filename);
+
+    use YAML;
+
+    print Dump($self->{xml_simple});
+
+    0;
+}
+
+
+sub qualify
+{
+    my $self = shift;
+
+    my $filename = shift;
+
+    my $result = $self->{model_container}->{neurospaces}->NeurospacesQualifyFilename($filename);
+
+    return $result;
+}
+
+
+1;
+
 

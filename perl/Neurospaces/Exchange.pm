11e34e2a10259193c61c7e8b171fa50e68f0085d
Hugo Cornelis <hugo.cornelis@gmail.com>
2010-03-31 13:53:13 +0000

    1. Fixed a warning generated during neuroml vs nineml detection.

--- perl/Neurospaces/Exchange.pm
+++ perl/Neurospaces/Exchange.pm
@@ -273,7 +273,7 @@ sub is_neuroml
 {
     my $self = shift;
 
-    return $self->{xml_simple}->{"xsi:schemaLocation"} =~ /neuroml/i;
+    return (defined $self->{xml_simple}->{"xsi:schemaLocation"}) and ($self->{xml_simple}->{"xsi:schemaLocation"} =~ /neuroml/i);
 }
 
 
